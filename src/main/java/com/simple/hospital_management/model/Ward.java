0f21050092925291bd404d6067f9881d857f75a2
FrostScythe <maheshwariayush371@gmail.com>
2025-08-07 19:46:21 +0530

    Updating model class and setting up controller class

--- src/main/java/com/simple/hospital_management/model/Ward.java
+++ src/main/java/com/simple/hospital_management/model/Ward.java
@@ -2,4 +2,48 @@ package com.simple.hospital_management.model;
 
 public class Ward {
     private int id;
+    private String type;
+    private int totalBed;
+    private int occupiedBed;
+    private int emptyBed;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getTotalBed() {
+        return totalBed;
+    }
+
+    public void setTotalBed(int totalBed) {
+        this.totalBed = totalBed;
+    }
+
+    public int getOccupiedBed() {
+        return occupiedBed;
+    }
+
+    public void setOccupiedBed(int occupiedBed) {
+        this.occupiedBed = occupiedBed;
+    }
+
+    public int getEmptyBed() {
+        return emptyBed;
+    }
+
+    public void setEmptyBed(int emptyBed) {
+        this.emptyBed = emptyBed;
+    }
 }
