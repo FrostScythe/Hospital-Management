4082112703ac6d011dce99c49f5777d0863dc304
FrostScythe <maheshwariayush371@gmail.com>
2025-08-08 11:42:47 +0530

    Doctor controller complete

--- src/main/java/com/simple/hospital_management/controller/DoctorController.java
+++ src/main/java/com/simple/hospital_management/controller/DoctorController.java
@@ -1,4 +1,25 @@
 package com.simple.hospital_management.controller;
 
+import com.simple.hospital_management.model.Doctor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/doctor/api")
+
 public class DoctorController {
+    // Hashmap - using hashmap to sto re the data -in memory
+
+    Map<Integer, Doctor> doctorMapDb = new HashMap<>();
+
+    @PostMapping("/save")
+    public String saveDoctor(Doctor doctor){
+        doctorMapDb.put(doctor.getId(), doctor);
+        System.out.println("doctor saved: "+doctorMapDb);
+        return "Doctor saved successfully!";
+    }
 }
--- src/main/java/com/simple/hospital_management/controller/PatientController.java
+++ src/main/java/com/simple/hospital_management/controller/PatientController.java
@@ -1,4 +1,9 @@
 package com.simple.hospital_management.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patient/api")
 public class PatientController {
 }
